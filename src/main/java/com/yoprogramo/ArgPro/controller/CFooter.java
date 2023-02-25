5511005910d9b8354c88c52bdb4f251bc6839e8c
Luciafeliu <feliulucia0@gmail.com>
2023-02-24 22:01:10 -0300

    para deploy koyeb

--- src/main/java/com/yoprogramo/ArgPro/controller/CFooter.java
+++ src/main/java/com/yoprogramo/ArgPro/controller/CFooter.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("footer")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://luciafeliuportfolio.web.app/")
 public class CFooter {
     
     @Autowired
